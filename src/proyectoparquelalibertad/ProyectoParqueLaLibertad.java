12efe1abf65c5a8c8728c77dc0683d1a120abffb
LuisDanielCL <LuisDanielCL@Daniel>
2016-03-24 20:47:04 -0600

    Nuevo nuevo commit de prueba

--- src/proyectoparquelalibertad/ProyectoParqueLaLibertad.java
+++ src/proyectoparquelalibertad/ProyectoParqueLaLibertad.java
@@ -14,8 +14,6 @@ public class ProyectoParqueLaLibertad {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO 
-    }
+
     
 }
